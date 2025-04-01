a9ae2702b512bae7782ab52a1e84b7ba0016a83e
janekeyes <janekeyes123@gmail.com>
2025-04-01 14:39:17 +0100

    jane adding age validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
 class User < ApplicationRecord
     validates :username, :first_name, :last_name, :email, :job_role, presence: true
     validates :date_of_birth, presence: true, on: :create
+    validate :age_validation, on: :create
+
+    private 
+
+    def age_validation
+        return if date_of_birth.blank?
+
+    age = ((Date.today - date_of_birth) / 365.25).to_i
+    if age < 18
+      errors.add(:date_of_birth, "Employees cannot be under 18 years of age.")
+    elsif age > 70
+      errors.add(:date_of_birth, "Employees cannot be over 70 years of age.")
+    end
+  end
+
   end
   
