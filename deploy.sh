42ece6b7c4a50faf489916b26c044fa1c5720572
janekeyes <janekeyes123@gmail.com>
2025-04-01 13:46:41 +0100

    jane fixing indentation

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# Update package list and install Node.js and npm
+# Update package list and install dependencies
 sudo apt update && sudo apt install -y nodejs npm
 
 # Install pm2, a process manager for Node.js
 sudo npm install -g pm2
 
 # Stop any running instance of the application
-pm2 stop employees 
+pm2 stop employees || true
 
 # Install application dependencies
 npm install || true
